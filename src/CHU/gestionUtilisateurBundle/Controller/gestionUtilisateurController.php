100bb8e1033da7548fe68b052e47721ca92276bf
JulienFontaine <fonfonjuju49@gmail.com>
2017-10-11 16:32:04 +0200

    prototype d'application de la  gestion d'utilisateur

--- src/CHU/gestionUtilisateurBundle/Controller/gestionUtilisateurController.php
+++ src/CHU/gestionUtilisateurBundle/Controller/gestionUtilisateurController.php
@@ -9,28 +9,28 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
+use CHU\gestionUtilisateurBundle\Entity\Utilisateur;
+
 class gestionUtilisateurController extends Controller
 {
-    // PARAMETRES DE LOGIN
-    const PARAM_USERNAME = 'uname';
-    const PARAM_PASSWORD = 'psw';
-
-    // PARAMETRES DE CREATION COMPTE
-    const PARAM_LASTNAME_USER = "nom";
-    const PARAM_FIRSTNAME_USER = "prenom";
+    // PARAMETRES
+    const PARAM_USERNAME = 'username';
+    const PARAM_PASSWORD = 'password';
+    const PARAM_NOM = "nom";
+    const PARAM_PRENOM = "prenom";
+    const PARAM_FONCTION = "fonction";
+    const PARAM_DATE_NAISSANCE = "dateNaissance";
+    const PARAM_ADRESSE_MAIL = "adresseMail";
+    const PARAM_CONFIRM_PASSWORD = "confirmPassword";
 
     // VUES
-    const VUE_PAGE_LOGIN = 'CHUgestionUtilisateurBundle:LoginPages:login.html.twig';
-    const VUE_PAGE_CREATION_UTILISATEUR = 'CHUgestionUtilisateurBundle:CreationUtilisateur:creationUtilisateur.html.twig';
-    const VUE_PAGE_ACCUEIL = 'CHUgestionUtilisateurBundle:PagesUtilisateur:accueil.html.twig';
-
-    // VARIABLES VUES
-    const VAR_USERNAME = 'username';
-    const VAR_PASSWORD = 'password';
-    const VAR_FIRSTNAME = 'firstname';
-    const VAR_LASTNAME = 'lastname';
+    const VUE_PAGE_LOGIN = 'CHUgestionUtilisateurBundle:gestionUtilisateur:login.html.twig';
+    const VUE_PAGE_CREATION_UTILISATEUR = 'CHUgestionUtilisateurBundle:gestionUtilisateur:enregistrement.html.twig';
+    const VUE_PAGE_ACCUEIL = 'CHUgestionUtilisateurBundle:gestionUtilisateur:accueil.html.twig';
+    const VUE_PAGE_RECUPERATION_MDP = 'CHUgestionUtilisateurBundle:gestionUtilisateur:recuperation_mdp.html.twig';
 
     // ROUTES
+    const ROUTE_PAGE_ACCUEIL = 'chu_gestion_utilisateur_accueil';
     const ROUTE_PAGE_LOGIN = 'chu_gestion_utilisateur_page_login';
 
     // MESSAGES
@@ -41,7 +41,7 @@ class gestionUtilisateurController extends Controller
      * Affiche la page de login
      */
     {
-      $content = $this->get('templating')->render(self::VUE_PAGE_LOGIN);
+      $content = $this->get('templating')->render(self::VUE_PAGE_ACCUEIL);
       return new Response($content);
     }
 
@@ -54,6 +54,25 @@ class gestionUtilisateurController extends Controller
       return new Response($content);
     }
 
+    public function pageLoginAction(Request $request)
+    /*
+     * Affiche la page de création d'utilisateur
+     */
+    {
+
+      $content = $this->get('templating')->render(self::VUE_PAGE_LOGIN);
+      return new Response($content);
+    }
+
+    public function recuperationAction()
+    /*
+     * Affiche la page de récupération de mot de passe
+     */
+    {
+      $content = $this->get('templating')->render(self::VUE_PAGE_RECUPERATION_MDP);
+      return new Response($content);
+    }
+
     public function creationUtilisateurAction(Request $request)
     /*
      * Gère la création de compte d'un utilisateur
@@ -62,18 +81,27 @@ class gestionUtilisateurController extends Controller
       if($request->isMethod('POST')){
         $username = $request->request->get(self::PARAM_USERNAME);
         $password = $request->request->get(self::PARAM_PASSWORD);
-        $firstname = $request->request->get(self::PARAM_FIRSTNAME_USER);
-        $lastname = $request->request->get(self::PARAM_LASTNAME_USER);
+        $confirm_password = $nom = $request->request->get(self::PARAM_CONFIRM_PASSWORD);
+        $prenom = $request->request->get(self::PARAM_PRENOM);
+        $nom = $request->request->get(self::PARAM_NOM);
+        $date_naissance = $request->request->get(self::PARAM_DATE_NAISSANCE);
+        $adresse_mail = $request->request->get(self::PARAM_ADRESSE_MAIL);
+        $fonction = $request->request->get(self::PARAM_FONCTION);
 
-        $param = array(
-          self::VAR_USERNAME => $username,
-          self::VAR_PASSWORD => $password,
-          self::VAR_FIRSTNAME => $firstname,
-          self::VAR_LASTNAME => $lastname
-        );
+        $user = new Utilisateur();
+        $user->setUsername($username)
+             ->setPassword($password)
+             ->setPrenom($prenom)
+             ->setNom($nom)
+             ->setDateNaissance($date_naissance)
+             ->setAdresseMail($adresse_mail)
+             ->setFonction($fonction);
 
-        $content = $this->get('templating')->render(self::VUE_PAGE_ACCUEIL,$param);
-        return new Response($content);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        return $this->redirectToRoute(self::ROUTE_PAGE_ACCUEIL);
       }
 
       throw new NotFoundHttpException(self::NOT_LOGGED_MESSAGE);
@@ -88,13 +116,18 @@ class gestionUtilisateurController extends Controller
         $username = $request->request->get(self::PARAM_USERNAME);
         $password = $request->request->get(self::PARAM_PASSWORD);
 
-        $param = array(
-          self::VAR_USERNAME => $username,
-          self::VAR_PASSWORD => $password
-        );
+        $utilisateur_repo = $this->getDoctrine()->getManager()->getRepository('CHUgestionUtilisateurBundle:Utilisateur');
 
-        $content = $this->get('templating')->render(self::VUE_PAGE_ACCUEIL,$param);
-        return new Response($content);
+        $user = $utilisateur_repo->findOneBy(array(self::PARAM_USERNAME => $username, self::PARAM_PASSWORD => $password));
+
+        if($user === null){
+          $session = $request->getSession();
+          $session->getFlashBag()->add('info',"Erreur lors de l'identification");
+          return $this->redirectToRoute(self::ROUTE_PAGE_LOGIN);
+        }
+
+        return $this->redirectToRoute(self::ROUTE_PAGE_ACCUEIL);
+
       }
 
       throw new NotFoundHttpException("Vous devez vous connecter d'abord");
@@ -103,7 +136,7 @@ class gestionUtilisateurController extends Controller
     public function deconnectionAction(Request $request)
     {
       if($request->isMethod('POST')){
-        return $this->redirectToRoute(self::ROUTE_PAGE_LOGIN);
+        return $this->redirectToRoute(self::ROUTE_PAGE_ACCUEIL);
       }
 
       throw new NotFoundHttpException(self::NOT_LOGGED_MESSAGE);
